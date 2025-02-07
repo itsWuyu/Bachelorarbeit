28b51845a6a124cdfcf27c0c86fd029daccc7f8e
Jens Römmling <itsWuyu@gmail.com>
2025-02-07 14:38:54 +0100

    Alle AuditLogs in 30 Tage von Benutzer XY

---
+++ AuditLogs.ps1
@@ -0,0 +1,22 @@
+$UserEmail = "benutzer@domain.de"
+$Operations = @("FileDownloaded", "FileAccessed", "FileDeleted", "FileCopied", "SharingSet", "FolderMoved", "FolderDeleted")
+
+$AuditLogs = Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-300) -EndDate (Get-Date) -UserIds $UserEmail -RecordType SharePointFileOperation -Operations $Operations |
+ForEach-Object {
+    $AuditData = $_.AuditData | ConvertFrom-Json
+    [PSCustomObject]@{
+        CreationDate = $_.CreationDate
+        UserIds = $_.UserIds
+        Operations = $_.Operations
+        FilePath = $AuditData.ObjectId
+        IP = $AuditData.ClientIP
+        GeoLocation = $AuditData.GeoLocation
+        UserAgent = $AuditData.UserAgent
+    }
+}
+#Daten Anzeigen lassen
+$AuditLogs  | Sort-Object CreationDate -Descending |
+Select-Object CreationDate, UserIds, Operations, FilePath, IP, GeoLocation, UserAgent |
+Format-Table -Wrap -AutoSize
+# Export als JSON Datei
+$AuditLogs | ConvertTo-Json -Depth 3 | Out-File "AuditLogData.json"
